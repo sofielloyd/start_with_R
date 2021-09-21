d4a7fd8808f2237391c21da5b3f72494e5188ea5
sofielloyd <89076017+sofielloyd@users.noreply.github.com>
2021-09-21 15:19:47 +0200

    Delete start_with_R.R

--- start_with_R.R
+++
@@ -1,9 +0,0 @@
-rooms <- c(1, 2, 1, 3, 1, NA, 3, 1, 3, 2, 1, NA, 1, 8, 3, 1, 4, NA, 1, 3, 1, 2, 1, 7, 1, NA)
-
-#Vector greater than 2
-rooms_no_na <- na.omit(rooms)
-rooms_above_2 <- rooms_no_na[rooms_no_na > 2]
-length(rooms_above_2)
-
-#Median
-median(rooms, na.rm = TRUE)
